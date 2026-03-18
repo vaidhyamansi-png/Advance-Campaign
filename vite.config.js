8a48384537d55df67964a01b9054ceeee6d5661d
Mansi Vaidhya <mansivaidhya@bitcot.com>
2026-03-18 18:08:23 +0530

    code changes done

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import laravel from 'laravel-vite-plugin'
 import { fileURLToPath } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -10,16 +11,19 @@ import MetaLayouts from 'vite-plugin-vue-meta-layouts'
 import vuetify from 'vite-plugin-vuetify'
 import svgLoader from 'vite-svg-loader'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Advance-Campaign/',  // ✅ Fixed
-
-  plugins: [
+  
+  plugins: [// Docs: https://github.com/posva/unplugin-vue-router
+  // ℹ️ This plugin should be placed before vue plugin
     VueRouter({
       getRouteName: routeNode => {
+      // Convert pascal case to kebab case
         return getPascalCaseRouteName(routeNode)
           .replace(/([a-z\d])([A-Z])/g, '$1-$2')
           .toLowerCase()
       },
+
       routesFolder: 'resources/js/pages',
     }),
     vue({
@@ -27,33 +31,38 @@ export default defineConfig({
         compilerOptions: {
           isCustomElement: tag => tag === 'swiper-container' || tag === 'swiper-slide',
         },
+
         transformAssetUrls: {
           base: null,
           includeAbsolute: false,
         },
       },
     }),
-    // ❌ laravel() plugin removed
-    vueJsx(),
+    laravel({
+      input: ['resources/js/main.js'],
+      refresh: true,
+    }),
+    vueJsx(), // Docs: https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin
     vuetify({
       styles: {
         configFile: 'resources/styles/variables/_vuetify.scss',
       },
-    }),
+    }), // Docs: https://github.com/dishait/vite-plugin-vue-meta-layouts?tab=readme-ov-file
     MetaLayouts({
       target: './resources/js/layouts',
       defaultLayout: 'default',
-    }),
+    }), // Docs: https://github.com/antfu/unplugin-vue-components#unplugin-vue-components
     Components({
       dirs: ['resources/js/@core/components', 'resources/js/views/demos', 'resources/js/components'],
       dts: true,
       resolvers: [
         componentName => {
+        // Auto import `VueApexCharts`
           if (componentName === 'VueApexCharts')
             return { name: 'default', from: 'vue3-apexcharts', as: 'VueApexCharts' }
         },
       ],
-    }),
+    }), // Docs: https://github.com/antfu/unplugin-auto-import#unplugin-auto-import
     AutoImport({
       imports: ['vue', VueRouterAutoImports, '@vueuse/core', '@vueuse/math', 'vue-i18n', 'pinia'],
       dirs: [
@@ -64,6 +73,8 @@ export default defineConfig({
         './resources/js/plugins/*/composables/*',
       ],
       vueTemplate: true,
+
+      // ℹ️ Disabled to avoid confusion & accidental usage
       ignore: ['useCookies', 'useStorage'],
       eslintrc: {
         enabled: true,
@@ -72,9 +83,7 @@ export default defineConfig({
     }),
     svgLoader(),
   ],
-
   define: { 'process.env': {} },
-
   resolve: {
     alias: {
       '@core-scss': fileURLToPath(new URL('./resources/styles/@core', import.meta.url)),
@@ -89,19 +98,21 @@ export default defineConfig({
       '@api-utils': fileURLToPath(new URL('./resources/js/plugins/fake-api/utils/', import.meta.url)),
     },
   },
-
-build: {
-  outDir: 'dist',
-  chunkSizeWarningLimit: 5000,
-  rollupOptions: {
-    input: 'index.html',  // ✅ yeh add karo
+  build: {
+    chunkSizeWarningLimit: 5000,
   },
-},
-
-
   optimizeDeps: {
     exclude: ['vuetify'],
-    entries: ['./resources/js/**/*.vue'],
+    entries: [
+      './resources/js/**/*.vue',
+    ],
   },
+
+   base: '/Advance-Campaign/'
+
 })
+
+
+
+
 
